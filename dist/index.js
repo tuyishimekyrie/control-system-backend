2440b452cc7bfc0d638275c76ec192957d6e08ba
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-04 18:36:09 +0200

    modified gitignore configurations

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World!");
 });
 app.listen(4000, () => {
     console.log("Server is listening on port 4000");
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, {Request,Response} from "express";
 const app = express();
 
 app.get("/", (req:Request,res:Response) => {
-    res.send("Hello World")
+    res.send("Hello World!")
 })
 
 app.listen(4000, () => {
