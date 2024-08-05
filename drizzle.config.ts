a3b0a7df2dd5fc7bbe6912cfe3e343a395282cc2
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-05 12:22:48 +0200

    add port in the configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/models",
   out: "./drizzle/migrations",
-  dialect: "mysql", 
+  dialect: "mysql",
   dbCredentials: {
     host: process.env.DB_HOST!,
     user: process.env.DB_USER!,
     password: process.env.DB_PASSWORD!,
     database: process.env.DB_NAME!,
+      port: parseInt(process.env.DB_PORT!),
+    // ssl:true,
   },
 });
