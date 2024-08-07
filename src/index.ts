2f52a516915bdcb5ed7432ffbc30b8c18b6f32a3
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-07 15:42:13 +0200

    add urlencoded algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import express from "express";
 import { dbObj } from "../drizzle/db";
 import router from "./routes";
 import { logger } from "./utils/Logger";
-import cors from "cors"
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.port ;
+const port = process.env.port;
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("hello world");
