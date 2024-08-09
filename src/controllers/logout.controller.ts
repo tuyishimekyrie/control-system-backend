1a1fe2ac4c0ec0064762a37d12bfdc49a9aedcd2
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-09 10:19:12 +0200

    add functionality to remove authorization from the login session

--- src/controllers/logout.controller.ts
+++ src/controllers/logout.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 
 export const logoutController = (req: Request, res: Response) => {
+  res.removeHeader("Authorization");
+  res.clearCookie("authToken");
   res.send("Logout User");
 };
