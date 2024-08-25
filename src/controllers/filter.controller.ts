06f2602683d25b84365dbf477e8ffab3808d7a87
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-25 14:01:52 +0200

    implement functionality of blacklist/whitelist

---
+++ src/controllers/filter.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { dbObj as db } from "../../drizzle/db";
+import { Category, Keyword, URLFilter } from "../models";
+
+export const filterController = async (req: Request, res: Response) => {
+  try {
+    // Fetch categories with associated URLs and keywords
+    const categories = await (await db).select().from(Category).execute();
+    const keywords = await (await db).select().from(Keyword).execute();
+    const blockedURLs = await (await db).select().from(URLFilter).execute();
+
+    // Structure the data by categories
+    const responseData = categories.map((category) => {
+      const categoryKeywords = keywords
+        .filter((keyword) => keyword.id === category.id)
+        .map((keyword) => keyword.keyword);
+
+      const categoryURLs = blockedURLs
+        .filter((urlFilter) => urlFilter.id === category.id)
+        .map((urlFilter) => urlFilter.url);
+
+      return {
+        category: category.name,
+        keywords: categoryKeywords,
+        blockedURLs: categoryURLs,
+      };
+    });
+
+    res.status(200).json(responseData);
+  } catch (error) {
+    console.error("Error fetching filter data:", (error as Error).message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- src/models/keyword.ts
+++ src/models/keyword.ts
@@ -1,8 +1,10 @@
-import { mysqlTable, text, varchar } from "drizzle-orm/mysql-core";
+import { mysqlTable, serial, text, varchar } from "drizzle-orm/mysql-core";
+import { Category } from "./category";
 
 export const Keyword = mysqlTable("Keyword", {
   id: varchar("id", { length: 256 })
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
   keyword: varchar("name", { length: 256 }).notNull().unique(),
+  categoryId: serial("categoryId").references(() => Category.id),
 });
