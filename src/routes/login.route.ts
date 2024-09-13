1e95b59256b83a56f9ae25891644a2ffba569a0a
Aldot-02 <114801675+Aldot-02@users.noreply.github.com>
2024-09-13 23:43:29 +0200

    ft(Authentication): Adding reset password functionality (#31)
    Co-authored-by: Aldo Twizerimana <aldotwizerimana@Aldos-MacBook-Pro.local>

--- src/routes/login.route.ts
+++ src/routes/login.route.ts
@@ -1,8 +1,14 @@
 import express from "express";
-import { loginController } from "../controllers";
+import {
+  loginController,
+  updatePasswordController,
+  getUserByEmailController,
+} from "../controllers";
 
 const loginRoute = express.Router();
 
 loginRoute.post("/auth/login", loginController);
+loginRoute.patch("/auth/update-password", updatePasswordController);
+loginRoute.get("/user/:email", getUserByEmailController);
 
 export default loginRoute;
